8af70b37eb9b398f6ea26e92f9e8051983ba00a1
Gabriel Capella <gabriel@capellaresumo.com>
2013-09-02 00:36:30 -0300

    Mais mudanças...

--- static/javascripts/app.js
+++ static/javascripts/app.js
@@ -1,5 +1,6 @@
 	
-		    $.each(praias, function(i, item) {
+/*
+$.each(praias, function(i, item) {
         $('#praiaslista').append('<li class="arrow" idpraia="'+item.id+'"><strong>'+item.praia+'</strong><small>'+item.cidade+'</small></li>')
     });
     
@@ -64,3 +65,9 @@
         // fire the above change event after every letter
         $(this).change();
     });
+    
+    */
+
+$.each(praias,function(a,b){$("#praiaslista").append('<li class="arrow" idpraia="'+b.id+'"><strong>'+b.praia+"</strong><small>"+b.cidade+"</small></li>")});$("li").each(function(){$(this).click(function(){idp=$(this).attr("idpraia");$.post("http://pixel.capella.pro/cetesb/info.php",{id:idp}).done(function(a){showpraia(idp,a);console.log(idp)})})});
+function showpraia(a,b){var c=$.grep(praias,function(b){return b.id==a});console.log(c);console.log(a);$$("div #praianome").html(c[0].praia+" - "+c[0].cidade);$$("div #praialocal").html(c[0].local);0==b?Lungo.Router.article("form","form-normal"):Lungo.Router.article("form","form-normal2")}jQuery.expr[":"].Contains=function(a,b,c){return 0<=(a.textContent||a.innerText||"").toUpperCase().indexOf(c[3].toUpperCase())};
+$("#search").change(function(){var a=$(this).val();console.log(a);a?($("#praiaslista").find(":not(:Contains("+a+"))").parent().hide(),$("#praiaslista").find(":Contains("+a+")").parent().show()):$("#praiaslista").find("li").slideDown();return!1}).keyup(function(){$(this).change()});
